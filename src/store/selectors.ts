e176b271ae3ebad1dfdf5fad80a38b11cb16c981
Denis <chilikin.den@gmail.com>
2022-02-01 16:21:46 +0300

    коментарии

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -6,3 +6,19 @@ export const getPostsData = (state: ReducerType) => state.postReducer.posts;
 export const getUserData = (state: ReducerType) =>
   // @ts-ignore
   state.userRegistrationReducer.user;
+
+// Если делаю таким образом в типах,  ts не видит userRegistrationReducer
+
+// export type RedecerUserType = {
+//   userRegistrationReducer: {
+//     user: UserType;
+//   };
+// };
+
+// или так, то тоже не выходит
+
+// export type RedecerUser = {
+//    userRegistrationReducer: RedecerUserType;
+// };
+
+// аналогичная ситуация и с постами
